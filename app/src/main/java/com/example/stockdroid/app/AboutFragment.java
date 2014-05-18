9901f3d97e9f067840538df68b2933456c3136e5
Tim Wong <tim.wong1992@gmail.com>
2014-05-18 18:32:44 -0400

    initial commit for about page

---
+++ app/src/main/java/com/example/stockdroid/app/AboutFragment.java
@@ -0,0 +1,12 @@
+package com.example.stockdroid.app;
+
+import android.app.Fragment;
+
+/**
+ * Fragment to display information about the application.
+ *
+ * Created by tim on 5/18/14.
+ */
+public class AboutFragment extends Fragment {
+
+}
