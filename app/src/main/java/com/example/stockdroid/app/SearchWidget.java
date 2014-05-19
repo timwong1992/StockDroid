6b7432a9f0fcc4236c7781971def89d6d5d14341
Tim Wong <tim.wong1992@gmail.com>
2014-05-19 07:05:49 -0400

    xml and java class updates to search widget

--- app/src/main/java/com/example/stockdroid/app/SearchWidget.java
+++ app/src/main/java/com/example/stockdroid/app/SearchWidget.java
@@ -1,7 +1,28 @@
 package com.example.stockdroid.app;
 
+import android.content.Context;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
 /**
  * Created by tim on 5/19/14.
  */
-public class SearchWidget {
+public class SearchWidget extends View {
+    private final EditText searchEditText;
+    private final Button searchButton;
+
+    public SearchWidget(Context context) {
+        super(context);
+        searchEditText = (EditText) findViewById(R.id.searchEditText);
+        searchButton = (Button) findViewById(R.id.searchButton);
+    }
+
+    public EditText getSearchEditText() {
+        return searchEditText;
+    }
+
+    public Button getSearchButton() {
+        return searchButton;
+    }
 }
